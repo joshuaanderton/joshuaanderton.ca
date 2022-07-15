cd7a8401d56dfc7ac80eb6aab4895893e7dd67c0
Joshua Anderton <info@joshanderton.com>
2022-07-15 07:02:15 -0700

    Force https on prod/stag

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Features\Pages;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+if (App::environment(['production', 'staging'])) :
+  URL::forceScheme('https');
+endif;
 
 Route::domain(config('subdomains.music'))->group(function ($router) {
   Route::get('/', Pages\Music::class)->name('music');
