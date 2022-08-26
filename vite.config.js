cdf08c49dc987b13c7329fb9cf1ae98dc5f75a52
Joshua Anderton <info@joshanderton.com>
2022-08-26 10:18:24 -0700

    Updated vite

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
-import laravel from 'laravel-vite-plugin'
 import preact from '@preact/preset-vite'
-import tailwindcss from 'tailwindcss'
+import laravel from 'laravel-vite-plugin'
 import blazervel from './vendor/blazervel/ui/vite.config'
 
 export default blazervel({
   plugins: [
     preact(),
-    tailwindcss(),
     laravel(['resources/js/app.tsx'])
   ]
 })
