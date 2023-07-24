0c40a4b8153a8a1e93340356038da925958136e0
Joshua Anderton <info@joshanderton.com>
2023-07-23 17:03:30 -0700

    YAML-ize homepage data

---
+++ content/collections/jobs/polarize-technologies-inc.md
@@ -0,0 +1,7 @@
+---
+id: 5123d3ef-8809-47bc-a73a-a2d58b63d9fb
+blueprint: job
+title: 'Polarize Technologies Inc.'
+updated_by: c2206a2f-e06b-4f8f-923f-3f97c7f9e6a9
+updated_at: 1689262349
+---
--- resources/js/site.js
+++ resources/js/site.js
@@ -1,6 +1,16 @@
 import Alpine from 'alpinejs'
+import websocketsConnection from './utils/websockets-connection'
 import '../css/site.css'
- 
+
 window.Alpine = Alpine
 
 Alpine.start()
+
+window.Echo = websocketsConnection()
+
+window.Echo
+  .channel('todos')
+  .listen('TodoCreated', (data) => console.log(data))
+  .error(err => console.log(err))
+
+window.createTodo = (message) => fetch(`/api/todos/create?message=${message}`)
